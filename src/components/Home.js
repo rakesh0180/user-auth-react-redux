cf09663bb0d47bfdacc15602cb8be32f8ab1211d
Rakesh.M <rakeshraki0180@gmail.com>
2021-10-09 12:27:31 +0530

    intial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,33 +1,43 @@
-import React from 'react'
-import '../App.css'
+import React from "react";
+import "../App.css";
 
 const Home = (props) => {
-    return (
-        <div>
-            <div className='imgContainer'>
-                <img src='https://res.cloudinary.com/practicaldev/image/fetch/s--7y7OVurK--/c_imagga_scale,f_auto,fl_progressive,h_500,q_auto,w_1000/https://i1.wp.com/blog.mallow-tech.com/wp-content/uploads/2016/12/user-authentication.jpg%3Fresize%3D820%252C400' alt='user-authentication' className='logo'/>
-            </div>
-            <div className='content'>
-                <div className='leftContent'>
-                    <p>
-                        <b>
-                            User authentication is a security process that covers all of the human-to-computer interactions that require the user to register and log in. Said more simply, authentication asks each user, “who are you?” and verifies their response.
-                        </b>
-                    </p>
-                </div>
-                <div className='rightContent'>
-                    <p>
-                        <b>
-                            How do I authenticate a user?<br />
-                            The process is fairly simple, users input their credentials on the website's login form. That information is then sent to the authentication server where the information is compared with all the user credentials on file. When a match is found, the system will authenticate users and grant them access to their accounts
-                        </b>
-                    </p>
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="imgContainer">
+        <img
+          src="https://cdn-bpplm.nitrocdn.com/MJefRwORvlGzblepEIRIgrqQFCGliGiG/assets/static/optimized/rev-e747184/wp-content/uploads/2018/02/authentication-vs-authorization.png  "
+          alt="user-authentication"
+          className="logo"
+        />
+      </div>
+      <div className="content">
+        <div className="leftContent">
+          <p>
+            <b>
+              User authentication is a security process that covers all of the
+              human-to-computer interactions that require the user to register
+              and log in. Said more simply, authentication asks each user, “who
+              are you?” and verifies their response.
+            </b>
+          </p>
         </div>
-        
-        
-    )
-}
+        <div className="rightContent">
+          <p>
+            <b>
+              How do I authenticate a user?
+              <br />
+              The process is fairly simple, users input their credentials on the
+              website's login form. That information is then sent to the
+              authentication server where the information is compared with all
+              the user credentials on file. When a match is found, the system
+              will authenticate users and grant them access to their accounts
+            </b>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,58 +1,62 @@
-import React from 'react'
-import { Link, Route, withRouter } from 'react-router-dom'
-import { useSelector, useDispatch } from 'react-redux'
-import Home from './Home'
-import Login from './Login'
-import Register from './Register'
-import Account from './Account'
-import toggleStatus from '../actions/toggleStatus'
-import MyNotes from './MyNotes'
-import swal from 'sweetalert'
-import '../App.css'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link, Route, withRouter } from "react-router-dom";
+import swal from "sweetalert";
+import toggleStatus from "../actions/toggleStatus";
+import "../App.css";
+import Account from "./Account";
+import Home from "./Home";
+import Login from "./Login";
+import MyNotes from "./MyNotes";
+import Register from "./Register";
 
 const NavBar = (props) => {
+  const userLoggedIn = useSelector((state) => state.userLoggedIn);
+  const dispatch = useDispatch();
 
-    const userLoggedIn = useSelector(state => state.userLoggedIn)
-    const dispatch = useDispatch()
+  return (
+    <div>
+      <div className="links">
+        <Link to="/">Home</Link> ||
+        {userLoggedIn ? (
+          <>
+            <Link to="/account">Account</Link> ||
+            <Link to="/mynotes">MyNotes</Link> ||
+            <Link
+              to="/"
+              onClick={() => {
+                localStorage.removeItem("token");
+                dispatch(toggleStatus());
+                props.history.push("/");
+                swal("Cool!", "You have logged out successfully!", "success");
+              }}
+            >
+              Logout
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link to="/register">Register</Link> ||
+            <Link to="/login">Login</Link>
+          </>
+        )}
+      </div>
 
-    return (
-        <div>
-            <div className='links'>
-                <Link to='/'>Home</Link> || 
-                {userLoggedIn ? (
-                    <>
-                        <Link to='/account'>Account</Link> ||
-                        <Link to='/mynotes'>MyNotes</Link> ||
-                        <Link to='/' onClick={() => {
-                            localStorage.removeItem('token')
-                            dispatch(toggleStatus())
-                            props.history.push('/')
-                            swal("Cool!", "You have logged out successfully!", "success")
-                        }}>Logout</Link>
-                    </>
-                ) : (
-                    <>
-                        <Link to='/register'>Register</Link> ||
-                        <Link to='/login'>Login</Link>
-                    </>
-                )}
-
-            </div>
-
-            <Route path='/' component={Home} exact={true} />
-            <Route path='/register' component={Register} />
-            <Route path='/login' render={(props) => {
-                return <Login 
-                            {...props}
-                        />
-            }}/>
-            <Route path='/account' component={Account}/>
-            <Route path='/mynotes' component={MyNotes} />
-        </div>
-    )
-}
+      <Route path="/" component={Home} exact={true} />
+      <Route path="/register" component={Register} />
+      <Route
+        path="/login"
+        render={(props) => {
+          return <Login {...props} />;
+        }}
+      />
+      <Route path="/account" component={Account} />
+      <Route path="/mynotes" component={MyNotes} />
+    </div>
+  );
+};
 
 // const WrappedComponent = withRouter(NavBar)
 // export default WrappedComponent
 
-export default withRouter(NavBar)
+export default withRouter(NavBar);
